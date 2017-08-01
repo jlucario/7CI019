e45717b606f00960457ac10f09ffca75e884cd16
James Braznell <jamesbraznell@yahoo.co.uk>
2017-08-01 20:41:50 +0100

    My work

---
+++ passenger_insert.sql
@@ -0,0 +1,20 @@
+INSERT INTO PASSENGER VALUES
+(4000001, 'Joe', 'Cook', '21 Dunhampton Road', 'Male', 07658978644, 'Joe@yahoo.com');
+INSERT INTO PASSENGER VALUES
+(4000002, 'Sam', 'Roads', '34 Lingot Drive', 'Male', 01562962748, NULL);				
+INSERT INTO PASSENGER VALUES
+(4000003, 'Ann', 'Kale', '7 Magistrate Road', 'Female', 07897346736, 'Ann@gmail.com');
+INSERT INTO PASSENGER VALUES
+(4000004, 'Elizabeth', 'Tiles', '19 Lake Road', 'Female', 01734566390, 'Elizabeth@hotmail.com');
+INSERT INTO PASSENGER VALUES
+(4000005, 'John', 'Wick', '11 Waterfall Drive', 'Male', 07439678378, NULL);
+INSERT INTO PASSENGER VALUES
+(4000006, 'Lisa', 'Stone', '27 Weavers Wharf', 'Female', 07863654674, 'Lisa@Outlook.com');
+INSERT INTO PASSENGER VALUES
+(4000007, 'Emma', 'Moody', '42 Wolverley Road', 'Female', 08446789455, 'Emma@yahoo.com');
+INSERT INTO PASSENGER VALUES
+(4000008, 'Harry', 'Alter', '15 Chester Wharf', 'Male', 08734595473, 'Harry@yahoo.com');
+INSERT INTO PASSENGER VALUES
+(4000009, 'Frank', 'Newey', '30 Birmingham Drive', 'Male', 03868752343, NULL);
+INSERT INTO PASSENGER VALUES
+(4000010, 'Maria', 'Smith', '3 Churchill Street', 'Female', 07438768244, 'Maria@hotmail.com');
---
+++ ship_insert.sql
@@ -0,0 +1,17 @@
+INSERT INTO Ship
+values(100001,'Mary Ann', 'Athens', 27000);
+
+INSERT INTO Ship
+values(100002, 'The Intrepid', 'Rome', 43000);
+
+INSERT INTO Ship
+values(100003, 'Sturgeon', 'Naples', 31200);
+
+INSERT INTO Ship
+values(100004, 'Achilles', 'Nice', 59600);
+
+INSERT INTO Ship
+values(100005, 'Odyssey', 'Rome', 34000);
+
+INSERT INTO Ship
+values(100006, 'Gemini', 'Naples', 49500);
